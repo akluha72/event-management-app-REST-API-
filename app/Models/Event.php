0a5ef4aa485c23eaa49d20a928bcc4f0f180263c
lutfi_akmal <lutfiakmal@myra.com.my>
2024-08-07 15:50:36 +0800

    Creating The Rest API project SETUP

--- app/Models/Event.php
+++ app/Models/Event.php
@@ -3,9 +3,24 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Factories\BelongsTo;
+
 use Illuminate\Database\Eloquent\Model;
+
+// use App\Models\Attendee;
+// use App\Models\User;
 
 class Event extends Model
 {
     use HasFactory;
+
+    public function user(): BelongsTo
+    {
+       return $this->belongsTo(User::class);
+    }
+
+    public function attendee(): HasMany
+    {
+       return $this->hasMany(Attendee::class);
+    }
 }
